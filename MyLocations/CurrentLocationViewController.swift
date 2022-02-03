26cfc1286bc75f7df4939a8350f07e829301379d
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-03 17:42:08 -0500

    First tab UI with constraints

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel:  UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    //MARK: - Actions
+    @IBAction func getLocation() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
