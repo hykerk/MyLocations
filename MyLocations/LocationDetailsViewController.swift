34c4f4439c6f5db2acc8ea1c67e49ca8a509ed37
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-04 17:31:38 -0500

    Display information on the Tag Location screen

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -27,4 +27,42 @@ class LocationDetailsViewController: UITableViewController {
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        dateLabel.text = format(date: Date())
+    }
+    
+    //MARK: - Helper Methods
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        if let tmp = placemark.subThoroughfare {
+            text += tmp + " "
+        }
+        if let tmp = placemark.thoroughfare {
+            text += tmp + ", "
+        }
+        if let tmp = placemark.locality {
+            text += tmp + ", "
+        }
+        if let tmp = placemark.administrativeArea {
+            text += tmp + " "
+        }
+        if let tmp = placemark.postalCode {
+            text += tmp + ", "
+        }
+        if let tmp = placemark.country {
+            text += tmp
+        }
+        return text
+    }
 }
