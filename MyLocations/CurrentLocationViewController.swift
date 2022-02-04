4de68a17345b652085e031fe3f97b3684471f10d
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-04 02:21:50 -0500

    Display coordinates

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -18,6 +18,8 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     
     let locationManager = CLLocationManager()
     
+    var location: CLLocation?
+    
     //MARK: - Actions
     @IBAction func getLocation() {
         let authStatus = locationManager.authorizationStatus
@@ -41,6 +43,22 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("didUpdateLocation \(newLocation)")
+        location = newLocation
+        updateLabels()
+    }
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start"
+        }
     }
     
     //MARK: - Helper Methods
