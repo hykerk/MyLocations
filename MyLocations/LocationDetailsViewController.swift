cc478a2c96694cd714c4e475583b3c41c8b7a273
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-04 17:16:20 -0500

    Display location info

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
 class LocationDetailsViewController: UITableViewController {
     @IBOutlet var descriptionTextView: UITextView!
@@ -15,6 +16,9 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet var longitudeLabel: UILabel!
     @IBOutlet var addressLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    
+    var coordinate = CLLocationCoordinate2D (latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
     
     //MARK: - Actions
     @IBAction func done() {
