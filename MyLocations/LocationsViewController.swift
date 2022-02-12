80c96e494d745c6b88a68dea90933f3d831abb98
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-12 18:01:15 -0500

    delete locations

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -60,6 +60,17 @@ class LocationsViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let location = fetchedResultsController.object(at: indexPath)
+            managedObjectContext.delete(location)
+            do {
+                try managedObjectContext.save()
+            } catch {
+                fatalCoreDataError(error)
+            }
+        }
+    }
     //MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "EditLocation" {
