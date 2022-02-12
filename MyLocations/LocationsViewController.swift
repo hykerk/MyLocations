c7dbc50fb91b48e7769663d7d878dfc3fb5475a1
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-12 00:12:12 -0500

    create custom table view cell subclass

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -36,27 +36,9 @@ class LocationsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath) as! LocationCell
         let location  = locations[indexPath.row]
-        let descriptionLabel = cell.viewWithTag(100) as! UILabel
-        descriptionLabel.text = location.locationDescription
-        let addressLabel = cell.viewWithTag(101) as! UILabel
-        if let placemark = location.placemark {
-            var  text = ""
-            if let tmp = placemark.subThoroughfare {
-                text += tmp + " "
-            }
-            if let tmp = placemark.thoroughfare {
-                text += tmp + ", "
-            }
-            if let tmp = placemark.locality {
-                text += tmp
-            }
-            addressLabel.text = text
-        } else {
-            addressLabel.text = ""
-        }
-        
+        cell.configure(for: location)
         return cell
     }
 }
