2b8872bfcc5d32a9c6e2452537f5baf5dfb81245
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-12 22:54:21 -0500

    use  photo library to add image

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -163,7 +163,8 @@ class LocationDetailsViewController: UITableViewController {
         if indexPath.section == 0 && indexPath.row == 0 {
             descriptionTextView.becomeFirstResponder()
         } else if indexPath.section == 1 && indexPath.row == 0 {
-            takePhotoWithCamera()
+            //takePhotoWithCamera()
+            choosePhotoFromLibrary()
         }
     }
 }
@@ -173,6 +174,13 @@ extension LocationDetailsViewController: UIImagePickerControllerDelegate, UINavi
     func takePhotoWithCamera() {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func choosePhotoFromLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
         present(imagePicker, animated: true, completion: nil)
