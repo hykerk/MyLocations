bf8f57c38a192e39d9452e7174e6b86db24b19bc
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-12 22:24:16 -0500

    create custom annotations

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -48,6 +48,37 @@ class MapViewController: UIViewController {
         return mapView.regionThatFits(region)
     }
     
+    func mapView(_ mapView: MKMapView, viewFor  annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Location else {
+            return nil
+        }
+        let identifier = "Location"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        if annotationView == nil {
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            pinView.isEnabled = true
+            pinView.canShowCallout = true
+            pinView.animatesDrop  = false
+            pinView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1)
+            let rightButton = UIButton(type: .detailDisclosure)
+            rightButton.addTarget(self, action: #selector(showLocationDetails(_:)), for: .touchUpInside)
+            pinView.rightCalloutAccessoryView = rightButton
+            annotationView = pinView
+        }
+        if let annotationView = annotationView {
+            annotationView.annotation = annotation
+            let button = annotationView.rightCalloutAccessoryView as! UIButton
+            if let index = locations.firstIndex(of: annotation as! Location) {
+                button.tag = index
+            }
+        }
+        return annotationView
+    }
+    
+    @objc func showLocationDetails(_ sender: UIButton) {
+        
+    }
+    
     //MARK: - Actions
     @IBAction func showUser() {
         let region = MKCoordinateRegion(center:mapView.userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
