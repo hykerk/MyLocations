db314a0c4446940a59b789a46fd8e37a0f4f08e0
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-12 22:12:11 -0500

    Show  a region

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -18,6 +18,34 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLocations()
+        if !locations.isEmpty {
+            showLocations()
+        }
+    }
+    
+    func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
+        let region: MKCoordinateRegion
+        switch annotations.count {
+        case 0:
+            region = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        case 1:
+            let annotation = annotations[annotations.count - 1]
+            region = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        default:
+            var topLeft = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+            var bottomRight = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+            for annotation in annotations {
+                topLeft.latitude = max(topLeft.latitude, annotation.coordinate.latitude)
+                topLeft.longitude = min(topLeft.longitude, annotation.coordinate.longitude)
+                bottomRight.latitude = min(bottomRight.latitude, annotation.coordinate.latitude)
+                bottomRight.longitude = max(bottomRight.longitude, annotation.coordinate.longitude)
+            }
+            let center = CLLocationCoordinate2D(latitude: topLeft.latitude - (topLeft.latitude - bottomRight.latitude) / 2, longitude: topLeft.longitude  - (topLeft.longitude - bottomRight.longitude) / 2)
+            let extraSpace = 1.1
+            let span = MKCoordinateSpan(latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace, longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace)
+            region = MKCoordinateRegion(center: center, span: span)
+        }
+        return mapView.regionThatFits(region)
     }
     
     //MARK: - Actions
@@ -26,7 +54,8 @@ class MapViewController: UIViewController {
         mapView.setRegion(mapView.regionThatFits(region), animated: true)
     }
     @IBAction func showLocations() {
-        
+        let theRegion = region(for: locations)
+        mapView.setRegion(theRegion, animated: true)
     }
     //MARK: - Helper Delegates
     func updateLocations() {
