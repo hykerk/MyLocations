135143371975e541f1b9ceef25968cb5b5438ec8
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-13 00:17:04 -0500

    convert place marks to strings

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -29,15 +29,9 @@ class LocationCell: UITableViewCell {
         }
         if let placemark = location.placemark {
             var text = ""
-            if let tmp = placemark.subThoroughfare {
-                text += tmp + ""
-            }
-            if let tmp = placemark.thoroughfare {
-                text += tmp + ", "
-            }
-            if let tmp = placemark.locality {
-                text += tmp
-            }
+            text.add(text: placemark.subThoroughfare)
+            text.add(text: placemark.thoroughfare, separatedBy: " ")
+            text.add(text: placemark.locality, separatedBy: ", ")
             addressLabel.text = text
         } else {
             addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
@@ -46,7 +40,7 @@ class LocationCell: UITableViewCell {
     }
     func thumbnail(for location: Location) -> UIImage {
         if location.hasPhoto, let image = location.photoImage {
-            return image
+            return image.resized(withBounds: CGSize(width: 52, height: 52))
         }
         return UIImage()
     }
