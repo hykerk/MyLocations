96b42985b6fe8290ee0a529f4ea4fc7a675517b5
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-12 23:32:29 -0500

    closures and capturing

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -62,11 +62,13 @@ class LocationDetailsViewController: UITableViewController {
         tableView.reloadData()
     }
     func listenForBackgroundNotification() {
-        observer = NotificationCenter.default.addObserver(forName: UIScene.didEnterBackgroundNotification, object: nil, queue: OperationQueue.main) {_ in
-            if self.presentedViewController != nil {
-                self.dismiss(animated: false, completion: nil)
+        observer = NotificationCenter.default.addObserver(forName: UIScene.didEnterBackgroundNotification, object: nil, queue: OperationQueue.main) { [weak self] _ in
+            if let weakSelf = self {
+                if weakSelf.presentedViewController != nil {
+                    weakSelf.dismiss(animated: false, completion: nil)
+                }
+                weakSelf.descriptionTextView.resignFirstResponder()
             }
-            self.descriptionTextView.resignFirstResponder()
         }
     }
     deinit {
